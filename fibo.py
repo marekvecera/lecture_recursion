c40db53cb7b02459fa05881d9ec547b6ffc678e8
marekvecera <veceramarek02@gmail.com>
2024-04-22 11:56:06 +0200

    fibo ready

--- fibo.py
+++ fibo.py
@@ -1,6 +1,11 @@
+def recursive_nth_fibo(n):
+    if n == 1 or n == 0:
+        return n
+    return recursive_nth_fibo(n-1) + recursive_nth_fibo(n-2)
+
 
 def main():
-    pass
+    print(recursive_nth_fibo(7))
 
 
 if __name__ == "__main__":
